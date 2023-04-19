3121fa25144b4a660c2781a2fe75ca245ac54209
AlisonRob <jezitaroberts@gmail.com>
2023-04-19 19:35:44 -0300

    ESTRUCTURA INICIAL

---
+++ tp2_grupo2556/src/main/java/ar/edu/unju/fi/Tp2Grupo2556Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo2556Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo2556Application.class, args);
+	}
+
+}
